77fa220e410d37a64d79feae59c1c3c7b07fec7d
Biswojeet Panda <biswajeet15696@gmail.com>
2017-11-10 21:14:27 +0530

    initial commit

---
+++ Assets/Transparent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
